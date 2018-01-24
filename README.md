48da1a9d2136d3df228e403aa63d768b4855a7d1
Wenceslao Negrete <vampay@digitaldealers.mx>
2018-01-23 20:05:49 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pet Store
+
+[Work in progress]
+
+## Tareas
+### Procedimientos
+Evaluar si el código actual cumple con los estándares básicos de programación por procedimientos: ¿Es claro, legible y tiene funciones que se dedican a procesos específicos?
+
+### OOP
+* Añadir un objeto _Cart_ que te permita agregar, quitar o modificar los objetos seleccionados por el usuario.
+* Al dar click el botón comprar, el modal que aparece debe contener los datos de la compra(listado de items y precio total + iva) estos datos deben provenir del nuevo objeto _Cart_.
+* Evaluar si los listeners y las modificaciones al DOM merecen ser parte de un objeto. De ser así implementarlo con sus respectivos métodos y propiedades.
+* Evaluar si cada uno de los productos merece ser convertido en un objeto, si es así deberíamos usar un factory? Herencia prototipal o quizás hacer una composicion?
+
+### Programación Funcional
+* De los métodos antes desarrollados, ¿cuál de ellos podría tener una aproximación funcional? Recuerda que esta aproximación debe tomar en cuenta inmutabilidad, funciones puras y predecibles.
+* ¿Podríamos y/o deberíamos modificar todos nuestros objetos para implementar solamente FP?
---
+++ app.js
@@ -0,0 +1,87 @@
+var kitties = [
+  {
+    id: 'ky-0',
+    name: 'Kitty 1',
+    thumbnail: 'landing-kitty01.svg',
+    price: 150
+  },
+  {
+    id: 'ky-1',
+    name: 'Kitty 2',
+    thumbnail: 'landing-kitty03.svg',
+    price: 250
+  },
+  {
+    id: 'ky-2',
+    name: 'Kitty 3',
+    thumbnail: 'landing-kitty05.svg',
+    price: 250
+  },
+  {
+    id: 'ky-3',
+    name: 'Kitty 4',
+    thumbnail: 'landing-kitty07.svg',
+    price: 250
+  },
+  {
+    id: 'ky-4',
+    name: 'Kitty 5',
+    thumbnail: 'landing-kitty09.svg',
+    price: 250
+  },
+  {
+    id: 'ky-5',
+    name: 'Kitty 6',
+    thumbnail: 'landing-kitty11.svg',
+    price: 250
+  },
+  {
+    id: 'ky-6',
+    name: 'Kitty 7',
+    thumbnail: 'landing-kitty13.svg',
+    price: 250
+  },
+  {
+    id: 'ky-7',
+    name: 'Kitty 8',
+    thumbnail: 'landing-kitty15.svg',
+    price: 250
+  }
+];
+
+var order = [];
+
+function render() {
+  for (var i = 0; i < kitties.length; i++) {
+    var kitty = kitties[i];
+    $('.js-gallery-list').append(`
+    <li class="kitty ${kitty.id}">
+      <h3 class="title">${kitty.name}</h3>
+      <img src="https://www.cryptokitties.co/images/${kitty.thumbnail}"/>
+      <p>$${kitty.price}</p>
+    </li>
+    `);
+  }
+}
+
+function initListeners() {
+  $('.js-gallery-list').on('click', '.kitty', function () {
+    var id = $(this).attr('class').split(' ')[1];
+    console.log(id);
+  });
+  $('.js-btn-cart').on('click', function () {
+    $('.js-btn-close').removeClass('hidden');
+    $('.js-shopping-cart').removeClass('hidden');
+    $('.js-btn-cart').addClass('hidden');
+  });
+  $('.js-btn-close').on('click', function () {
+    $('.js-btn-cart').removeClass('hidden');
+    $('.js-shopping-cart').addClass('hidden');
+    $('.js-btn-close').addClass('hidden');
+  });
+}
+
+$(function () {
+  render();
+  initListeners();
+});
