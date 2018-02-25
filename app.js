c4047a8e604a7a48a8d9757c537c16bc7de852d9
memecast <=>
2018-02-25 15:45:34 -0600

    <3 Creando un factory para los gatitos y haciendo una copia mejorada del primer objeto de datos que nos da, este paso sustituye a la funcion addProperties de nuestro acercamiento por programación funcional T_T

--- app.js
+++ app.js
@@ -1,87 +1,80 @@
-var kitties = [
-  {
-    id: 'ky-0',
-    name: 'Kitty 1',
-    thumbnail: 'landing-kitty01.svg',
-    price: 150
-  },
-  {
-    id: 'ky-1',
-    name: 'Kitty 2',
-    thumbnail: 'landing-kitty03.svg',
-    price: 250
-  },
-  {
-    id: 'ky-2',
-    name: 'Kitty 3',
-    thumbnail: 'landing-kitty05.svg',
-    price: 250
-  },
-  {
-    id: 'ky-3',
-    name: 'Kitty 4',
-    thumbnail: 'landing-kitty07.svg',
-    price: 250
-  },
-  {
-    id: 'ky-4',
-    name: 'Kitty 5',
-    thumbnail: 'landing-kitty09.svg',
-    price: 250
-  },
-  {
-    id: 'ky-5',
-    name: 'Kitty 6',
-    thumbnail: 'landing-kitty11.svg',
-    price: 250
-  },
-  {
-    id: 'ky-6',
-    name: 'Kitty 7',
-    thumbnail: 'landing-kitty13.svg',
-    price: 250
-  },
-  {
-    id: 'ky-7',
-    name: 'Kitty 8',
-    thumbnail: 'landing-kitty15.svg',
-    price: 250
+/* 
+  Qué necesitamos!
+  Un factory de gatitos 
+  Un factory para el carrito 
+  Por qué usar un factory: https://stackoverflow.com/a/628959
+  * Sabemos las propiedades y metodos que deberían tener. 
+  * Será mas sencillo agregar las que el raw data no tiene aún en el caso de los gatos.
+*/
+/*CAT FACTORY*/
+
+function itemFactory (){
+  // haremos una copia de los objetos en kitties con otras propiedades, así no modificamos la data original.
+  var kitties = [
+    {
+      id: 'ky-0',
+      name: 'Kitty 1',
+      thumbnail: 'landing-kitty01.svg',
+      price: 150
+    },
+    {
+      id: 'ky-1',
+      name: 'Kitty 2',
+      thumbnail: 'landing-kitty03.svg',
+      price: 250
+    },
+    {
+      id: 'ky-2',
+      name: 'Kitty 3',
+      thumbnail: 'landing-kitty05.svg',
+      price: 250
+    },
+    {
+      id: 'ky-3',
+      name: 'Kitty 4',
+      thumbnail: 'landing-kitty07.svg',
+      price: 250
+    },
+    {
+      id: 'ky-4',
+      name: 'Kitty 5',
+      thumbnail: 'landing-kitty09.svg',
+      price: 250
+    },
+    {
+      id: 'ky-5',
+      name: 'Kitty 6',
+      thumbnail: 'landing-kitty11.svg',
+      price: 250
+    },
+    {
+      id: 'ky-6',
+      name: 'Kitty 7',
+      thumbnail: 'landing-kitty13.svg',
+      price: 250
+    },
+    {
+      id: 'ky-7',
+      name: 'Kitty 8',
+      thumbnail: 'landing-kitty15.svg',
+      price: 250
+    }
+  ];
+  var catsObject = []; 
+  for(var i = 0 ; i < kitties.length; i ++){
+    catsObject.push(Object.assign({}, new Kitty(kitties[i])));
   }
-];
-
-var order = [];
-
-function render() {
-  for (var i = 0; i < kitties.length; i++) {
-    var kitty = kitties[i];
-    $('.js-gallery-list').append(`
-    <li class="kitty ${kitty.id}">
-      <h3 class="title">${kitty.name}</h3>
-      <img src="https://www.cryptokitties.co/images/${kitty.thumbnail}"/>
-      <p>$${kitty.price}</p>
-    </li>
-    `);
+  return catsObject;
+}
+function Kitty (item) {
+  this.id = item.id;
+  this.name = item.name;
+  this.thumbnail = item.thumbnail;
+  this.count = 0; 
+  this.price = item.price;
+  this.subtotal = function(){
+    return this.price * this.count;
   }
-}
-
-function initListeners() {
-  $('.js-gallery-list').on('click', '.kitty', function () {
-    var id = $(this).attr('class').split(' ')[1];
-    console.log(id);
-  });
-  $('.js-btn-cart').on('click', function () {
-    $('.js-btn-close').removeClass('hidden');
-    $('.js-shopping-cart').removeClass('hidden');
-    $('.js-btn-cart').addClass('hidden');
-  });
-  $('.js-btn-close').on('click', function () {
-    $('.js-btn-cart').removeClass('hidden');
-    $('.js-shopping-cart').addClass('hidden');
-    $('.js-btn-close').addClass('hidden');
-  });
-}
-
-$(function () {
-  render();
-  initListeners();
-});
+};
+const Kitties = itemFactory();
+console.log(Kitties);
